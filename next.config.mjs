bb35e79e219bf3aa98acc033047e98f2e8984ca6
Eduarda Magno <eduardagouveiamagno@gmail.com>
2024-09-18 15:37:37 +0100

    v2

--- next.config.mjs
+++ next.config.mjs
@@ -2,10 +2,11 @@ import { withContentlayer } from "next-contentlayer";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
-	experimental: {
-		mdxRs: true,
-	},
+  pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
+  output: 'export',  // Add this line to ensure the static export is activated
+  experimental: {
+    mdxRs: true,  // Keep this to support the experimental MDX processing
+  },
 };
 
 export default withContentlayer(nextConfig);
